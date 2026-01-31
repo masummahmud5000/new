cdc79348cd6132b208565a3ecc120350284fa2c3
Masum Mahmud <mdmasumpresent@gmail.com>
2026-01-31 19:36:11 +0600

    firts

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import './globals.css'
+
+const notFount = () => {
+
+    return(
+        <main className="not-found h-screen bg-[#bdb76b] text-shadow-1xs flex flex-col justify-center items-center">
+            <h1 className="text-8xl text-red-600">404</h1>
+            <h1 className="text-5xl text-amber-600">Page Not Found</h1>
+        </main>
+    )
+}
+export default notFount;
